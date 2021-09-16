3c61d9d73b0b57140ed01a4795bcd31dbe40998c
Parkgeunryeong <kkgk7@naver.com>
2021-09-17 04:33:22 +0900

    version0.6

--- syntax04.go
+++ syntax04.go
@@ -6,23 +6,25 @@ import (
 	"time"
 )
 
-// 난수 추출된 수의 소수 판정 프로그램 v0.4
+// 난수 추출된 수의 소수 판정 프로그램 v0.6
 // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
 func main() {
 	// seed 설정
 	seed := time.Now().Unix()
 	rand.Seed(seed)
-
+	//number = 21
 	isPrime := true
 	number := rand.Intn(150) + 2
+
 	fmt.Println("임의로 추출된 수 : ", number)
 
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
 			isPrime = false
+			break // 첫 번째 약수가 발견되면 반복문 즉시 종료
 
 		}
-		fmt.Print(i, " ")
+		//fmt.Print(i, " ")
 	}
 
 	if isPrime { // 비교 연산자 제거
